cb38d005926d20ca413182a3d04868d526e1f2d4
Marcelo Aguila <web@croop.cl>
2013-04-06 09:43:17 -0300

    Elimine la llamada a Jquery, uso el de foundation

--- functions.php
+++ functions.php
@@ -9,18 +9,6 @@
 	
 	// Add RSS links to <head> section
 	automatic_feed_links();
-	
-	// Load jQuery
-	if ( !function_exists(core_mods) ) {
-		function core_mods() {
-			if ( !is_admin() ) {
-				wp_deregister_script('jquery');
-				wp_register_script('jquery', ("http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"), false);
-				wp_enqueue_script('jquery');
-			}
-		}
-		core_mods();
-	}
 
 	// Clean up the <head>
 	function removeHeadLinks() {
