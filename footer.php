355a8ec2b6aa19c70f06c4ceabd42d044f9dc6b9
Marcelo Aguila <web@croop.cl>
2013-04-06 12:58:37 -0300

    Detalles Footer

--- footer.php
+++ footer.php
@@ -1,10 +1,10 @@
-		<footer >
-			<div class="row">
-				<div class="panel large-12 columns">
-					#Footer
-				</div>
-			</div>
-		</footer>
+<footer class="panel">
+	<div class="row">
+		<div class="large-12 columns">
+			<small>Creado por <a href="http://www.marceloaguila.com">Marcelo Aguila</a> de <a href="http://www.croop.cl">www.croop.cl</a></small>
+		</div>
+	</div>
+</footer>
 
 <?php wp_footer(); ?>
 
