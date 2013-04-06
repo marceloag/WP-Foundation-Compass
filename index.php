3dd321ea84afeadd891675f189ebf8773a6245bd
Marcelo Aguila <web@croop.cl>
2013-04-06 14:01:54 -0300

    Thumbnails en el home 5

--- index.php
+++ index.php
@@ -10,13 +10,13 @@
 			<div class="row">
 					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-					<article <?php post_class() ?> id="post-<?php the_ID(); ?>" class="row panel">
+					<article <?php post_class() ?> id="post-<?php the_ID(); ?>" class="row">
 
-						<small><?php include (TEMPLATEPATH . '/_/inc/meta.php' ); ?></small>
 						<figure class="large-3 columns"> 
 							 <?php the_post_thumbnail();?>
 						</figure>
 						<div class="entry large-9 columns">
+							<small><?php include (TEMPLATEPATH . '/_/inc/meta.php' ); ?></small>
 							<h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
 							<?php the_excerpt(); ?>
 						</div>
